f2e51b92c07219d77a0ad86e58d30728a300c405
Fernando Mauricio <fernando.mauricio@am.senac.br>
2025-09-03 16:54:56 -0400

    feat: atualizar configurações do Metro, Tailwind e TypeScript para melhor compatibilidade e organização

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,16 +1,16 @@
+// apps/mobile/metro.config.js
 const path = require('path');
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
-
 config.watchFolders = [workspaceRoot];
-
 config.resolver.nodeModulesPaths = [
-  path.join(projectRoot, 'node_modules'),
-  path.join(workspaceRoot, 'node_modules'),
+    path.join(projectRoot, 'node_modules'),
+    path.join(workspaceRoot, 'node_modules'),
 ];
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
